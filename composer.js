96511a47d7b575fb26447f18ec67311c078ab9a9
Makan <40735596+makannew@users.noreply.github.com>
2019-04-06 16:48:10 +0800

    handling hidden  metadata

--- composer.js
+++ composer.js
@@ -1,14 +1,14 @@
 
 export default function(){
   'use strict'
-
+  const compositeMetaData = Symbol.for("compositeMetaData")
+  const composite = {};
   let propNames = {};
   let liveFunctions = new Map();
   let updateStatus = {};
   // extract function parameters which should be in form of destructor object {para1 , para2 , ...}
   // x =  function({para1 , para2 , para3}) then para1 , para2 , para3 will match by Regular Expression
   const paraRegExp = /.*?\(\{([^)]*)\}\)/; 
-
   let runFunctions = async function(options , callNumber){
     Object.assign(updateStatus , options); // to keep track of the properties that needs to be updated
     let nextUpdates = {};
@@ -19,86 +19,79 @@ export default function(){
         if (!(updateStatus[item])){
             for (let method of liveFunctions.entries()){
             if (method[1].includes(item)){
-              if (method[1].reduce(function(previous , current){if (composit[current]===undefined){return false}else{return previous}}, true)){
-
-                resolvedMethod = await(method[0](composit , callNumber));
-                if (callNumber != composit.validAsyncCall) return false;
-                composit[method[0].name] = resolvedMethod;
+              if (method[1].reduce(function(previous , current){if (composite[current]===undefined){return false}else{return previous}}, true)){
+                resolvedMethod = await(method[0](composite , callNumber));
+                if (callNumber != composite[compositeMetaData].validAsyncCall) return false;
+                composite[method[0].name] = resolvedMethod;
                 nextUpdates[method[0].name] = false; 
               }else{
-                if (callNumber != composit.validAsyncCall) return false;
-                composit[method[0].name] = undefined;
+                if (callNumber != composite[compositeMetaData].validAsyncCall) return false;
+                composite[method[0].name] = undefined;
                 nextUpdates[method[0].name] = false;
               }
             }
           }
         }
       }
-      if (callNumber != composit.validAsyncCall){
+      if (callNumber != composite[compositeMetaData].validAsyncCall){
         return false;
       }else{
         updateStatus = {};
         Object.assign(updateStatus , nextUpdates);
       }
     }
-
-    if (callNumber != composit.validAsyncCall) {
+    if (callNumber != composite[compositeMetaData].validAsyncCall) {
       return false;
     }else{
-      composit.validAsyncCall = undefined;
-      if (composit.parentComposite){
+      composite[compositeMetaData].validAsyncCall = undefined;
+      if (composite[compositeMetaData].parentComposite){
         options={};
-        options[composit.parentComposite.affectedProp] = false;
-        composit.parentComposite.composit.validAsyncCall = Symbol();
-        composit.parentComposite.callUpdate(options , composit.parentComposite.composit.validAsyncCall);
+        options[composite[compositeMetaData].parentComposite.affectedProp] = false;
+        composite[compositeMetaData].parentComposite.composite[compositeMetaData].validAsyncCall = Symbol();
+        composite[compositeMetaData].parentComposite.callUpdate(options , composite[compositeMetaData].parentComposite.composite[compositeMetaData].validAsyncCall);
       }
       return true;
     }
   }
-
-  let composit = {parentComposite: undefined , validAsyncCall:undefined , runFunctions: runFunctions};
-
+  composite[compositeMetaData]= {parentComposite: undefined , validAsyncCall:undefined , runFunctions: runFunctions};
   let setProperties = function(options){
-    Object.assign(composit , options)
+    Object.assign(composite , options)
     for (let item in options){
       options[item] = false;
     }
-    composit.validAsyncCall = Symbol();
-    runFunctions(options , composit.validAsyncCall);
+    composite[compositeMetaData].validAsyncCall = Symbol();
+    runFunctions(options , composite[compositeMetaData].validAsyncCall);
     }
   let addFunction = function(method){
-    composit[method.name] = undefined;
+    composite[method.name] = undefined;
     propNames[method.name] = true;
     let functionPara = (method.toString().match(paraRegExp)[1]).split(',').map(item=>item.trim());
     functionPara.forEach(item => {
-      composit[item] = undefined;
+      composite[item] = undefined;
       propNames[item] = true;
     });
     liveFunctions.set(method , functionPara);
   }
   let addMethod = function(method){
-    composit[method.name] = method;
+    composite[method.name] = method;
   }
-
   let interceptor = function(affectedComposite , affectedProp){
     let nestedPropHandler = {
       get: function( obj , prop , receiver) {
         if (prop == "proxyType") return "interceptorProxy";
 
         if (typeof(obj[prop]) === "object" && obj[prop] != null) {
-          if (obj["parentComposite"]){
+          if (obj[compositeMetaData] && obj[compositeMetaData]["parentComposite"]){
             affectedComposite = obj;
             affectedProp = prop;
           }
-
           if (!(obj[prop]["proxyType"])){
             return new Proxy(Reflect.get(obj , prop , receiver ), nestedPropHandler);
           }
 
           if (obj[prop]["proxyType"]=="compositeProxy"){
-            obj[prop]["parentComposite"] = {callUpdate: runFunctions , affectedProp: affectedProp , composit: affectedComposite};
+            obj[prop][compositeMetaData]["parentComposite"] = {callUpdate: runFunctions , affectedProp: affectedProp , composite: affectedComposite};
             return new Proxy(Reflect.get(obj , prop , receiver ), nestedPropHandler);
-
           }
         }
         return Reflect.get(obj , prop , receiver );
@@ -109,8 +102,8 @@ export default function(){
         let options={};
         options[affectedProp] = false;
 
-        affectedComposite["validAsyncCall"] = Symbol();
-        affectedComposite["runFunctions"](options , affectedComposite["validAsyncCall"]);
+        affectedComposite[compositeMetaData]["validAsyncCall"] = Symbol();
+        affectedComposite[compositeMetaData]["runFunctions"](options , affectedComposite[compositeMetaData]["validAsyncCall"]);
         return true;
       }
     }
@@ -118,26 +111,21 @@ export default function(){
   }
   let compositHandler = {
     set: function ( obj , prop , value , receiver ){
-      if(prop == "addFunction" ||prop == "addMethod" || prop == "set" ) {
-        throw console.error("Cannot overwrite this property.");
-      }
-      if (!(prop in propNames) && prop != "parentComposite"&& prop != "validAsyncCall"){
+      if (!(prop in propNames)){
         throw console.error("Cannot create new property here");
       }
       if (typeof(value) === "object" && value != null){
         if (value["proxyType"]=="compositeProxy"){
-          value["parentComposite"] = {callUpdate: runFunctions , affectedProp: prop , composit:composit};
+          value[compositeMetaData]["parentComposite"] = {callUpdate: runFunctions , affectedProp: prop , composite:composite};
           Reflect.set(obj , prop , value["getComposite"] , receiver);
           return true;
         }
       }
-
       Reflect.set(obj , prop , value , receiver);
       let options={};
       options[prop] = false;
-      composit.validAsyncCall = Symbol();
-
-      runFunctions(options , composit.validAsyncCall);
+      composite[compositeMetaData].validAsyncCall = Symbol();
+      runFunctions(options , composite[compositeMetaData].validAsyncCall);
       return true;
     },
     get: function ( obj , prop , receiver ){
@@ -154,21 +142,17 @@ export default function(){
         return "compositeProxy"
       }
         if (typeof(obj[prop]) === "object" && obj[prop] != null){
-          if (obj["parentComposite"]){
+          if (obj[compositeMetaData] && obj[compositeMetaData]["parentComposite"]){
             return Reflect.get(obj , prop , receiver );
           }
-          
           if (!(obj[prop]["proxyType"])){
-
             return new Proxy(Reflect.get(obj , prop , receiver ), interceptor(obj , prop));
           }
 
         if (obj[prop]["proxyType"]=="compositeProxy"){
-          obj[prop]["parentComposite"] = {callUpdate: runFunctions , affectedProp: prop , composit:composit};
+          obj[prop][compositeMetaData]["parentComposite"] = {callUpdate: runFunctions , affectedProp: prop , composite:composite};
           return new Proxy(Reflect.get(obj , prop , receiver ), interceptor(obj , prop));
-
         }
-
       }
         return Reflect.get(obj , prop , receiver );
     },
@@ -177,15 +161,13 @@ export default function(){
         obj[prop] = undefined;
         let options={};
         options[prop] = false;
-        composit.validAsyncCall = Symbol();
-
-        runFunctions(options , composit.validAsyncCall);
+        composite[compositeMetaData].validAsyncCall = Symbol();
+        runFunctions(options , composite[compositeMetaData].validAsyncCall);
       }else{
         throw console.error("property not found.");
       }
     }
-
   }
-  let validateComposit = new Proxy(composit , compositHandler);
+  let validateComposit = new Proxy(composite , compositHandler);
   return validateComposit;
 }
